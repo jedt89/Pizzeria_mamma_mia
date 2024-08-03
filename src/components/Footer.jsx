06474c70b81601a78014c1814477f03e6b36403d
Jona <jonathan.diaz@driv.in>
2024-08-03 15:34:50 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,24 @@ const { Text } = Typography;
 import '../assets/styles/App.css';
 
 const Footer = () => {
+  const footerStyle = {
+    paddingBottom: '10px',
+    fontWeight: 'bold',
+    borderBottom: '1px solid #ffffff80',
+    marginBottom: '10px',
+    width: '100%',
+    maxWidth: '300px',
+    textAlign: 'center'
+  }
+
   return (
     <Flex vertical justify='center' align='center' className='footer'>
-      <Text>Jonathan Díaz</Text>
-      <Text>Desarrollador Front End</Text>
+      <Text style={{ fontWeight: 'bold' }}>Jonathan Díaz</Text>
+      <Text
+        style={footerStyle}
+      >
+        Desarrollador Front End
+      </Text>
       <Text>Desarrollo Full Stack</Text>
       <Text>Academia Desafío Latam</Text>
       <Text>2024</Text>
